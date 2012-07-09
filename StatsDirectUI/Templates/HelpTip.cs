5b06b2362e3cd43cfc64c999b71111ba9fabc052
Peter Crowther <peter.crowther@melandra.com>
2012-07-09 06:33:06 +0000

    Add missing file

---
+++ StatsDirectUI/Templates/HelpTip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace StatsDirect.Templates
+{
+    public class HelpTip
+    {
+        [XmlAttribute(AttributeName = "lang")]
+        public string Lang { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+}
