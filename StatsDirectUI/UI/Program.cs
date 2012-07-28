a223e3d1cbcca176c37c9442e056b2740ad46a9a
Peter Crowther <peter.crowther@melandra.com>
2012-07-28 12:46:58 +0000

    Remove unneeded using

--- StatsDirectUI/UI/Program.cs
+++ StatsDirectUI/UI/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.IO;
 using StatsDirect.Calculator;
-using StatsDirect.Expressions;
 using StatsDirect.Utilities;
 
 namespace StatsDirect.UI
