9d295ad71d047a43178627276524724db35b4cdc
Peter Crowther <peter.crowther@melandra.com>
2014-01-17 19:51:56 +0000

    Update to 2014 copyright and DevExpress 13.2

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("StatsDirect Limited")]
 [assembly: AssemblyProduct("StatsDirect")]
-[assembly: AssemblyCopyright("Copyright © 1997-2013 StatsDirect Limited")]
+[assembly: AssemblyCopyright("Copyright © 1997-2014 StatsDirect Limited")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.97.0")]
-[assembly: AssemblyFileVersion("3.0.97.0")]
+[assembly: AssemblyVersion("3.0.98.0")]
+[assembly: AssemblyFileVersion("3.0.98.0")]
 [assembly: CLSCompliant(false)]
