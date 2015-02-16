e751a09bb55b9c0582312d33be352ce1d6b10cb2
Peter Crowther <peter.crowther@melandra.com>
2015-02-16 12:11:00 +0000

    Reference latest .Net setup, prep for 3.1.140

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.139.0")]
-[assembly: AssemblyFileVersion("3.0.139.0")]
+[assembly: AssemblyVersion("3.0.140.0")]
+[assembly: AssemblyFileVersion("3.0.140.0")]
 [assembly: CLSCompliant(false)]
