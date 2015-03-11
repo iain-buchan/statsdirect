5b943cc73ce3b67de20e1f90723cdb7b47cac1de
Peter Crowther <peter.crowther@melandra.com>
2015-03-11 14:56:37 +0000

    Save an initial refactor of ctlOperationInput - incomplete

---
+++ StatsDirectUI/UI/ctlOperationInput.Designer.cs
@@ -0,0 +1,66 @@
+namespace StatsDirect.UI
+{
+    partial class ctlOperationInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlUser = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlUser
+            // 
+            this.pnlUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlUser.AutoSize = true;
+            this.pnlUser.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlUser.Location = new System.Drawing.Point(0, 0);
+            this.pnlUser.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlUser.MinimumSize = new System.Drawing.Size(1038, 0);
+            this.pnlUser.Name = "pnlUser";
+            this.pnlUser.Size = new System.Drawing.Size(1038, 0);
+            this.pnlUser.TabIndex = 0;
+            // 
+            // ctlOperationInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.pnlUser);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "ctlOperationInput";
+            this.Size = new System.Drawing.Size(1038, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlUser;
+    }
+}
