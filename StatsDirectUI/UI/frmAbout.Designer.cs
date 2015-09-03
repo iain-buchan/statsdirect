c2570d43b696da02c170bcda8e339afeec278981
Peter Crowther <peter.crowther@melandra.com>
2015-09-03 12:11:48 +0000

    Lint: Remove redundant designer attribute.

--- StatsDirectUI/UI/frmAbout.Designer.cs
+++ StatsDirectUI/UI/frmAbout.Designer.cs
@@ -170,7 +170,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.CancelButton = this.cmdOK;
             this.ClientSize = new System.Drawing.Size(423, 426);
             this.Controls.Add(this.pictureBox1);
