c31cba2857d8cabf21ea9ffeee50df8e3ad6c61a
Peter Crowther <peter.crowther@melandra.com>
2015-10-07 17:04:16 +0000

    Prepare for 3.0.157

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.155.0")]
-[assembly: AssemblyFileVersion("3.0.155.0")]
+[assembly: AssemblyVersion("3.0.157.0")]
+[assembly: AssemblyFileVersion("3.0.157.0")]
 [assembly: CLSCompliant(false)]
