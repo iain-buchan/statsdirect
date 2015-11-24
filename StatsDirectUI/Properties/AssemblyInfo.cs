2eea7db924351236d10783792737feb953ef930c
Peter Crowther <peter.crowther@melandra.com>
2015-11-24 14:33:58 +0000

    Clean up minor crud in AssemblyInfo.cs

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("StatsDirect")]
 [assembly: AssemblyDescription("StatsDirect statistical analysis workbench")]
 [assembly: AssemblyConfiguration("")]
@@ -14,21 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+// [assembly: Guid("cb054a8b-1fa0-4a28-b2ca-e857f6b5c9f8")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb054a8b-1fa0-4a28-b2ca-e857f6b5c9f8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("3.0.161.0")]
 [assembly: AssemblyFileVersion("3.0.161.0")]
 [assembly: CLSCompliant(false)]
