93de41af604f8a00a97d42b0a9383831aca2915b
Peter Crowther <peter.crowther@melandra.com>
2015-12-04 15:12:07 +0000

    #954: Separate out and clean up FunctionRegistry before implementing type safety.

---
+++ StatsDirectUI/Expressions/ArgumentDefinition.cs
@@ -0,0 +1,42 @@
+﻿namespace StatsDirect.Expressions
+{
+    public class ArgumentDefinition
+    {
+        public string ParameterName { get; set; }
+        public bool IsOptional { get; set; }
+        public string ParameterDefault { get; set; }
+
+        public ArgumentDefinition(string parameterName, bool isOptional, string parameterDefault)
+        {
+            ParameterName = parameterName;
+            IsOptional = isOptional;
+            ParameterDefault = parameterDefault;
+        }
+
+        public ArgumentDefinition(string parameterName)
+        {
+            ParameterName = parameterName;
+            IsOptional = false;
+            ParameterDefault = null;
+        }
+
+        public ArgumentDefinition(string parameterName, string parameterDefault)
+        {
+            ParameterName = parameterName;
+            IsOptional = true;
+            ParameterDefault = parameterDefault;
+        }
+
+        public override string ToString()
+        {
+            // Mandatory parameters only show their name.
+            if (!IsOptional)
+                return ParameterName;
+
+            // Optional parameters show different strings depending on whether or not they have a default.
+            if (null != ParameterDefault)
+                return string.Format("{0}:={1} (default)", ParameterName, ParameterDefault);
+            return ParameterName + " (optional)";
+        }
+    }
+}
--- StatsDirectUI/Expressions/FunctionDefinition.cs
+++ StatsDirectUI/Expressions/FunctionDefinition.cs
@@ -8,28 +8,7 @@ namespace StatsDirect.Expressions
     {
         public string Name { get; set; }
         public string ClrName { get; set; }
-        private readonly List<ArgumentDefinition> argumentDefinitions;
-
-        public FunctionDefinition(string name, string clrName, IList<string> parameterNamesAndDefaults)
-        {
-            Name = name;
-            ClrName = clrName;
-
-            if (null == parameterNamesAndDefaults)
-                return;
-
-            if (parameterNamesAndDefaults.Count % 2 != 0)
-                throw new ArgumentException("Must contain pairs of name, default", "parameterNamesAndDefaults");
-
-            argumentDefinitions = new List<ArgumentDefinition>(parameterNamesAndDefaults.Count / 2);
-
-            for (int i = 0; i < parameterNamesAndDefaults.Count; i += 2)
-            {
-                string parameterName = parameterNamesAndDefaults[i];
-                string parameterDefault = parameterNamesAndDefaults[i + 1];
-                argumentDefinitions.Add(new ArgumentDefinition(parameterName, null != parameterDefault, parameterDefault));
-            }
-        }
+        public List<ArgumentDefinition> ArgumentDefinitions { get; private set; }
 
         public FunctionDefinition(string name, string clrName, IEnumerable<ArgumentDefinition> argumentDefinitions)
         {
@@ -39,57 +18,12 @@ namespace StatsDirect.Expressions
             if (null == argumentDefinitions)
                 return;
 
-            this.argumentDefinitions = new List<ArgumentDefinition>(argumentDefinitions);
-        }
-
-        public List<ArgumentDefinition> ArgumentDefinitions
-        {
-            get { return argumentDefinitions; }
+            ArgumentDefinitions = new List<ArgumentDefinition>(argumentDefinitions);
         }
 
         public override string ToString()
         {
-            return Name + "(" + string.Join(", ", argumentDefinitions.Select(x=>x.ToString()).ToArray()) + ")";
-        }
-    }
-
-    public class ArgumentDefinition
-    {
-        public string ParameterName { get; set; }
-        public bool IsOptional { get; set; }
-        public string ParameterDefault { get; set; }
-
-        public ArgumentDefinition(string parameterName, bool isOptional, string parameterDefault)
-        {
-            ParameterName = parameterName;
-            IsOptional = isOptional;
-            ParameterDefault = parameterDefault;
-        }
-
-        public ArgumentDefinition(string parameterName)
-        {
-            ParameterName = parameterName;
-            IsOptional = false;
-            ParameterDefault = null;
-        }
-
-        public ArgumentDefinition(string parameterName, string parameterDefault)
-        {
-            ParameterName = parameterName;
-            IsOptional = true;
-            ParameterDefault = parameterDefault;
-        }
-
-        public override string ToString()
-        {
-            // Mandatory parameters only show their name.
-            if (!IsOptional)
-                return ParameterName;
-
-            // Optional parameters show different strings depending on whether or not they have a default.
-            if (null != ParameterDefault)
-                return string.Format("{0}:={1} (default)", ParameterName, ParameterDefault);
-            return ParameterName + " (optional)";
+            return Name + "(" + string.Join(", ", ArgumentDefinitions.Select(x=>x.ToString()).ToArray()) + ")";
         }
     }
 }
