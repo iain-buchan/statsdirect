0be4e6e66eaf8939f4dc8b56c38ee1b1bf287a1e
Peter Crowther <peter.crowther@melandra.com>
2015-12-31 00:05:18 +0000

    Prepare 3.0.163

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 // [assembly: Guid("cb054a8b-1fa0-4a28-b2ca-e857f6b5c9f8")]
 
-[assembly: AssemblyVersion("3.0.162.0")]
-[assembly: AssemblyFileVersion("3.0.162.0")]
+[assembly: AssemblyVersion("3.0.163.0")]
+[assembly: AssemblyFileVersion("3.0.163.0")]
 [assembly: CLSCompliant(false)]
