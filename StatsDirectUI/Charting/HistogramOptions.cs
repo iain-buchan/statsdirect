c24eae82345f845ac34ba68b577e4356b7b5f767
Peter Crowther <peter.crowther@melandra.com>
2016-07-22 09:52:57 +0000

    Tidy histogram options

--- StatsDirectUI/Charting/HistogramOptions.cs
+++ StatsDirectUI/Charting/HistogramOptions.cs
@@ -1,77 +1,8 @@
-using StatsDirect.Numerics;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace StatsDirect.Charting
 {
-    [Serializable]
-    public class HistogramSeriesOptions
-    {
-        public string ChartTitle { get; set; }
-        public string XAxisTitle { get; set; }
-        public string YAxisTitle { get; set; }
-
-        public BinsDescriptor BinsDescriptor { get; set; }
-
-        /// <summary>
-        /// Calculate minimum bin midpoint, midpoint interval and number of bins given the current state of the options.
-        /// If PoolVariablesForBins is true, this combines the data for all the variables.
-        /// If PoolVariablesForBins is false, this uses just the data from the series at seriesIndex.
-        /// </summary>
-        /// <param name="calculateBinCount">If true, force a full calculation of the number of bins.  If false, use the user-entered number of bins as a hint.</param>
-        /// <param name="binsFromUser">The user-entered number of bins</param>
-        /// <param name="series"> </param>
-        public void Reset(bool calculateBinCount, int binsFromUser, Series series)
-        {
-            //  TODO: Set up the global minimum and maximum values based on the series
-
-            //  Work out the values
-            double min;
-            double max;
-            DoubleSeries s = series.AsDoubleSeries;
-            min = s.Min;
-            max = s.Max;
-            BinsDescriptor descriptor = Calculate(s, binsFromUser, calculateBinCount);
-            BinsDescriptor = descriptor;
-        }
-
-        ///  <summary>
-        ///  
-        ///  </summary>
-        ///  <param name="series">Input data. All data will be pooled for the purposes of calculating minimum and maximum values.</param>
-        ///  <param name="binsFromUser">A user-entered bin count.</param>
-        ///  <param name="calculateBinCount">If false, use the user-entered bin count.  If true, calculate from scratch.</param>
-        /// <remarks></remarks>
-        public BinsDescriptor Calculate(Series series, int binsFromUser, bool calculateBinCount)
-        {
-            int actualRows;
-            double[] nonMissingData = ExtractNonMissingDataAndSort(series, out actualRows);
-
-            if (calculateBinCount || binsFromUser <= 1)
-                return HistogramBinChooser.ChooseBins(nonMissingData, 0, actualRows);
-            else
-            {
-                double[] edges = HistogramBinChooser.Linspace(nonMissingData[0], nonMissingData[actualRows - 1], binsFromUser);
-                int[] counts = HistogramBinChooser.SortedHist(nonMissingData, edges);
-                return new BinsDescriptor { Edges = edges, Counts = counts };
-            }
-        }
-
-        public static double[] ExtractNonMissingDataAndSort(Series series, out int actualRows)
-        {
-            double[] data = series.AsDoubleSeries.Data;
-            double[] nonMissingData = new double[data.Length];
-            actualRows = 0;
-            for (int c = 0; c < data.Length; c++)
-                if (data[c] != Constant.MISSING)
-                    nonMissingData[actualRows++] = data[c];
-            //  By now, nonMissingData(0) to nonMissingData(actualRows - 1) contain the actual data for the longest row, with missing data excluded.
-            Array.Sort(nonMissingData, 0, actualRows);
-            return nonMissingData;
-        }
-    }
-
     [Serializable]
     public class HistogramOptions : GenericOptions
     {
@@ -107,8 +38,7 @@ namespace StatsDirect.Charting
             HistoSeriesOptions[seriesIndex].Reset(calculateBinCount, binsFromUser, series);
 
             // Warn listeners that we've just changed our scale.
-            if (null != ScaleChanged)
-                ScaleChanged(this, EventArgs.Empty);
+            ScaleChanged?.Invoke(this, EventArgs.Empty);
         }
 
         public bool ShowRelativeFrequencies
@@ -122,19 +52,14 @@ namespace StatsDirect.Charting
                 bool changed = value != showRelativeFrequencies;
                 showRelativeFrequencies = value;
                 if (changed)
-                {
-                    if (null != ScaleChanged)
-                        ScaleChanged(this, EventArgs.Empty);
-                }
+                    ScaleChanged?.Invoke(this, EventArgs.Empty);
             }
         }
 
         //  Display options
         public int LineWidth { get; set; }
         public string AxisFontDescriptor { get; set; }
-
         public List<HistogramSeriesOptions> HistoSeriesOptions { get; set; }
-
         [field: NonSerialized]
         public event EventHandler ScaleChanged;
 
---
+++ StatsDirectUI/Charting/HistogramSeriesOptions.cs
@@ -0,0 +1,70 @@
+using StatsDirect.Numerics;
+using System;
+
+namespace StatsDirect.Charting
+{
+    [Serializable]
+    public class HistogramSeriesOptions
+    {
+        public string ChartTitle { get; set; }
+        public string XAxisTitle { get; set; }
+        public string YAxisTitle { get; set; }
+
+        public BinsDescriptor BinsDescriptor { get; set; }
+
+        /// <summary>
+        /// Calculate minimum bin midpoint, midpoint interval and number of bins given the current state of the options.
+        /// If PoolVariablesForBins is true, this combines the data for all the variables.
+        /// If PoolVariablesForBins is false, this uses just the data from the series at seriesIndex.
+        /// </summary>
+        /// <param name="calculateBinCount">If true, force a full calculation of the number of bins.  If false, use the user-entered number of bins as a hint.</param>
+        /// <param name="binsFromUser">The user-entered number of bins</param>
+        /// <param name="series"> </param>
+        public void Reset(bool calculateBinCount, int binsFromUser, Series series)
+        {
+            //  Work out the values
+            double min;
+            double max;
+            DoubleSeries s = series.AsDoubleSeries;
+            min = s.Min;
+            max = s.Max;
+            BinsDescriptor descriptor = Calculate(s, binsFromUser, calculateBinCount);
+            BinsDescriptor = descriptor;
+        }
+
+        ///  <summary>
+        ///  
+        ///  </summary>
+        ///  <param name="series">Input data. All data will be pooled for the purposes of calculating minimum and maximum values.</param>
+        ///  <param name="binsFromUser">A user-entered bin count.</param>
+        ///  <param name="calculateBinCount">If false, use the user-entered bin count.  If true, calculate from scratch.</param>
+        /// <remarks></remarks>
+        public BinsDescriptor Calculate(Series series, int binsFromUser, bool calculateBinCount)
+        {
+            int actualRows;
+            double[] nonMissingData = ExtractNonMissingDataAndSort(series, out actualRows);
+
+            if (calculateBinCount || binsFromUser <= 1)
+                return HistogramBinChooser.ChooseBins(nonMissingData, 0, actualRows);
+            else
+            {
+                double[] edges = HistogramBinChooser.Linspace(nonMissingData[0], nonMissingData[actualRows - 1], binsFromUser);
+                int[] counts = HistogramBinChooser.SortedHist(nonMissingData, edges);
+                return new BinsDescriptor { Edges = edges, Counts = counts };
+            }
+        }
+
+        public static double[] ExtractNonMissingDataAndSort(Series series, out int actualRows)
+        {
+            double[] data = series.AsDoubleSeries.Data;
+            double[] nonMissingData = new double[data.Length];
+            actualRows = 0;
+            for (int c = 0; c < data.Length; c++)
+                if (data[c] != Constant.MISSING)
+                    nonMissingData[actualRows++] = data[c];
+            //  By now, nonMissingData[0] to nonMissingData[actualRows - 1] contain the actual data for the longest row, with missing data excluded.
+            Array.Sort(nonMissingData, 0, actualRows);
+            return nonMissingData;
+        }
+    }
+}
