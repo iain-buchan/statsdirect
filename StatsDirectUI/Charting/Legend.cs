17a4c4c9ff11d8a08cf90beaf5cc5fa2b3558af4
Peter Crowther <peter.crowther@melandra.com>
2017-04-05 16:41:29 +0000

    Update SSG and DX

---
+++ StatsDirectUI/Charting/Legend.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace StatsDirect.Charting
+{
+    /// <summary>
+    /// A sequence of items to be drawn at some point on a chart as a legend.
+    /// </summary>
+    public class Legend
+    {
+        public IList<LegendEntry> LegendEntries { get; private set; }
+
+        public Legend()
+        {
+            LegendEntries = new List<LegendEntry>();
+        }
+    }
+
+    /// <summary>
+    /// One item in a Legend.
+    /// </summary>
+    public class LegendEntry
+    {
+        public MarkerType MarkerType { get; set; }
+        public string Label { get; set; }
+    }
+}
---
+++ StatsDirectUI/Charting/Renderer/NotSetChartRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StatsDirect.Templates;
+
+namespace StatsDirect.Charting.Renderer
+{
+    class NotSetChartRenderer : AbstractChartRenderer, IChartRenderer
+    {
+        public NotSetChartRenderer(ChartDefinition definition, ICanvasFactory canvasFactory)
+            : base(definition, canvasFactory)
+        {
+        }
+
+        ScaleParameters IChartRenderer.GetScaleParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        ParameterBag IChartRenderer.Plot(ITemplateHost host)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
