decb0ff5ad5bddf555c020d406f8c0e6116fe63c
Peter Crowther <peter.crowther@melandra.com>
2017-04-05 16:43:42 +0000

    Fix up various problems with casts to ChartRenderer with NotSetChartRenderer

--- StatsDirectUI/Charting/ChartRendererFactory.cs
+++ StatsDirectUI/Charting/ChartRendererFactory.cs
@@ -19,9 +19,8 @@ namespace StatsDirect.Charting
 
         public static string PlotBiasMAAndReturnRtf(ITemplateHost host, double[] x, double[] yy, double[] yw, int rows, string xtxt, double[] cl, double[] cu, double cco, double cit, double rmh, Transformation xform, bool diagonal)
         {
-            ChartDefinition cd = new ChartDefinition();
-            cd.ScaleParameters.X.ScaleType = ScaleType.Linear;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) };
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.Plot_Bias_MA(host, x, yy, yw, rows, xtxt, cl, cu, cco, cit, rmh, xform, diagonal);
                 return Render(ch);
@@ -30,9 +29,8 @@ namespace StatsDirect.Charting
 
         public static string PlotCorrelationAndReturnRtf(int k, string[] title, double[] odr, double[] odrl, double[] odru, double[] gn, CorrelationRowType[] pg, string cap, string qid, Transformation xform, bool isDifference)
         {
-            ChartDefinition cd = new ChartDefinition();
-            cd.ScaleParameters.X.ScaleType = Transformation.Log == xform ? ScaleType.Log10 : ScaleType.Linear;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(Transformation.Log == xform ? ScaleType.Log10 : ScaleType.Linear, ScaleType.Linear) };
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotCorrelation(k, title, odr, odrl, odru, gn, pg, cap, qid, xform, isDifference);
                 return Render(ch);
@@ -41,10 +39,10 @@ namespace StatsDirect.Charting
 
         public static string PlotCox2AndReturnRtf(int[] gn, int igroups, double[] xp, double[] yp, ColumnData[] cdat1, int groupid)
         {
-            ChartDefinition cd = new ChartDefinition();
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) };
             cd.AddXSeries(xp, null);
             cd.AddYSeries(yp, null);
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotCox2(gn, igroups, xp, yp, cdat1, groupid);
                 return Render(ch);
@@ -53,9 +51,8 @@ namespace StatsDirect.Charting
 
         public static string PlotEffectAndReturnRtf(ITemplateHost host, int k, double[] cn, double[] En, string[] title, double rmh, double ll, double ul, double cco, double[] odr, double[] odrl, double[] odru, string cap, int pbias, string qid)
         {
-            ChartDefinition cd = new ChartDefinition();
-            cd.ScaleParameters.X.ScaleType = ScaleType.Linear;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) };
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotEffect(host, k, cn, En, title, rmh, ll, ul, cco, odr, odrl, odru, cap, pbias, qid);
                 return Render(ch);
@@ -64,7 +61,7 @@ namespace StatsDirect.Charting
 
         public static string PlotLAbbeAndReturnRtf(int k, double[,] o, double rmh)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotLAbbe(k, o, rmh);
                 return Render(ch);
@@ -73,12 +70,12 @@ namespace StatsDirect.Charting
 
         public static string PlotLinearizedEstimationAndReturnRtf(double[] xData, double[] yData, string title, int model, double a, double b, string XAxisTitle, string YAxisTitle, bool shouldUseColour)
         {
-            ChartDefinition cd = new ChartDefinition();
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) };
             cd.AddYSeries(yData, YAxisTitle);
             cd.AddXSeries(xData, XAxisTitle);
             AgreementOptions aOptions = new AgreementOptions(shouldUseColour) { mxd = yData, av = xData };
             cd.ChartOptions = aOptions;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotLinearizedEstimation(title, model, a, b, XAxisTitle, YAxisTitle);
                 return Render(ch);
@@ -88,6 +85,7 @@ namespace StatsDirect.Charting
         public static string PlotLinearRegressionAndMaybeSeCiOrPredictionIntervalAndReturnRtf(double[] xData, double[] yData, string title, double slope, double intercept, bool fullWidth, string xAxisTitle, string yAxisTitle, double PERT, int nx, double MS, double SUMX, double SSX, bool isPredictionInterval)
         {
             ChartDefinition cd = new ChartDefinition();
+            cd.ChartType = ChartType.LinearRegression;
             DoubleSeries ys = new DoubleSeries(yData, yAxisTitle);
             cd.AddYSeriesAt(ys, 0);
             DoubleSeries xs = new DoubleSeries(xData, xAxisTitle);
@@ -119,11 +117,10 @@ namespace StatsDirect.Charting
 
         public static string PlotLogitAndReturnRtf(double[] x, double[] y, string title, int model, double t, double sw, double s1, double a, double b, string xAxisTitle, string yAxisTitle, bool useLogScale)
         {
-            ChartDefinition cd = new ChartDefinition();
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(useLogScale ? ScaleType.Log10 : ScaleType.Linear, ScaleType.Linear) };
             cd.AddYSeries(y, yAxisTitle);
             cd.AddXSeries(x, xAxisTitle);
-            cd.ScaleParameters.X.ScaleType = useLogScale ? ScaleType.Log10 : ScaleType.Linear;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotLogit(title, model, t, sw, s1, a, b, xAxisTitle, yAxisTitle);
                 return Render(ch);
@@ -132,9 +129,8 @@ namespace StatsDirect.Charting
 
         public static string PlotMHAndReturnRtf(int k, double[,] o, double[] odw, string[] title, double rmh, double ll, double ul, double cco, double[] odr, double[] odrl, double[] odru, bool[] lerr, bool[] uerr, string cap, int pbias, string qid)
         {
-            ChartDefinition cd = new ChartDefinition();
-            cd.ScaleParameters.X.ScaleType = ScaleType.Log10;
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            ChartDefinition cd = new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Log10, ScaleType.Linear) };
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.Plot_MH(k, o, odw, title, rmh, ll, ul, cco, odr, odrl, odru, lerr, uerr, cap, pbias, qid);
                 return Render(ch);
@@ -143,7 +139,7 @@ namespace StatsDirect.Charting
 
         public static string PlotMHRDAndReturnRtf(int k, double[,] o, double[] odw, string[] title, double rmh, double ll, double ul, double cco, double[] odr, double[] odrl, double[] odru, bool[] lerr, bool[] uerr, string cap, int pbias, string qid)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.Plot_MHRiskDifference(k, odw, title, rmh, ll, ul, cco, odr, odrl, odru, lerr, uerr, cap, pbias, qid);
                 return Render(ch);
@@ -166,10 +162,10 @@ namespace StatsDirect.Charting
         public static string PlotPolynomialRegressionAndReturnRtf(double[] xData, double[] yData, string title, int mode, double[,] xtxi, double[] bd, double rss, int nx, int P, double gamma, string xAxisTitle, string yAxisTitle, bool shouldUseColour)
         {
             AgreementOptions aOptions = new AgreementOptions(shouldUseColour) { mxd = yData, av = xData };
-            ChartDefinition cd = new ChartDefinition { ChartOptions = aOptions };
+            ChartDefinition cd = new ChartDefinition { ChartOptions = aOptions, ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) };
             cd.AddYSeries(yData, yAxisTitle);
             cd.AddXSeries(xData, xAxisTitle);
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(cd))
+            using (ChartRenderer ch = new ChartRenderer(cd, canvasFactory))
             {
                 ch.PlotPolynomialRegression(title, mode, xtxi, bd, rss, nx, P, gamma, xAxisTitle, yAxisTitle);
                 return Render(ch);
@@ -178,7 +174,7 @@ namespace StatsDirect.Charting
 
         public static string PlotTiesAndReturnMetafile(double[] x, double[] y, int nx, double lla, double ula, double GAMMA, string v0Title, string v1Title, double mean)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotTies(x, y, nx, lla, ula, GAMMA, v0Title, v1Title, mean);
                 return Render(ch);
@@ -187,7 +183,7 @@ namespace StatsDirect.Charting
 
         public static string PlotXYAndReturnRtf(double[] x, double[] y, string xtxt, string ytxt, string title, bool zPlot, DataMinMax minMaxY, bool useCalculatedScalesEvenWithDefinition)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotXY(x, y, xtxt, ytxt, title, zPlot, minMaxY, useCalculatedScalesEvenWithDefinition);
                 return Render(ch);
@@ -196,7 +192,7 @@ namespace StatsDirect.Charting
 
         public static string PlotXY0To1AndReturnRtf(double[] x, double[] y, string xtxt, string ytxt, string title, bool zPlot, DataMinMax minMaxY, bool useCalculatedScalesEvenWithDefinition)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotXY0To1(x, y, xtxt, ytxt, title, zPlot, minMaxY, useCalculatedScalesEvenWithDefinition);
                 return Render(ch);
@@ -205,7 +201,7 @@ namespace StatsDirect.Charting
 
         public static string PlotXYRAndReturnRtf(double[,] x, double[,,] y, int ng, int[] gn, int[,] nr, double[] b, double[] a, string xtxt, string ytxt, string title, string[] bnam, MinMax minMax)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotXYR(x, y, ng, gn, nr, b, a, xtxt, ytxt, title, bnam, minMax.MinX, minMax.MaxX, minMax.MinY, minMax.MaxY);
                 return Render(ch);
@@ -214,7 +210,7 @@ namespace StatsDirect.Charting
 
         public static string PlotXYZAndReturnRtf(double[] x, double[] y, double[] z, string xtxt, string ytxt, string title, bool zPlot, DataMinMax minMaxY)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotXYZ(x, y, z, 1, x.Length - 1, xtxt, ytxt, title, zPlot, minMaxY, new MarkerType { MarkerShape = MarkerShape.Circle, IsMarkerFilled = false, MarkerColor = AbstractChartRenderer.grBlack });
                 return Render(ch);
@@ -223,7 +219,7 @@ namespace StatsDirect.Charting
 
         public static string SurvivalOrHazardPlot(CoxP[] z, int iobs, int istrata, CoxPlotMode plotMode, int igroups, int groupid, bool grouped, bool stratified, double[,,] ARR3, ColumnData[] cdat1, bool use_tic, bool use_marker, int[] gn)
         {
-            using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+            using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
             {
                 ch.PlotCox1(z, iobs, istrata, plotMode, igroups, groupid, grouped, stratified, ARR3, cdat1, use_tic, use_marker, gn);
                 return Render(ch);
@@ -323,7 +319,7 @@ namespace StatsDirect.Charting
                     cnx[k] = nx;
                 }
                 // Plot the results
-                using (ChartRenderer ch = (ChartRenderer)ChartRendererFor(new ChartDefinition()))
+                using (ChartRenderer ch = new ChartRenderer(new ChartDefinition() { ScaleParameters = CreateScaleParameters(ScaleType.Linear, ScaleType.Linear) }, canvasFactory))
                 {
                     ch.x_plGraphInternal(dead, groups, cnx, glab, tic, marker, x, y, plotMode, xAxisTitle, yAxisTitle, title);
                     outputImages.Add(Render(ch));
@@ -373,13 +369,27 @@ namespace StatsDirect.Charting
                 case ChartType.Survival:
                     return new SurvivalChartRenderer(chartDefinition, canvasFactory);
                 default:
-                    return new ChartRenderer(chartDefinition, canvasFactory);
+                    return new NotSetChartRenderer(chartDefinition, canvasFactory);
             }
         }
 
         private static string Render(IChartRenderer ch)
         {
             return RtfImageRenderer.ImageStreamToRtf(ch.GetImageStream(), ch.ImageWidth, ch.ImageHeight);
+        }
+
+        private static string Render(ChartRenderer ch)
+        {
+            return RtfImageRenderer.ImageStreamToRtf(ch.GetImageStream(), ch.ImageWidth, ch.ImageHeight);
+        }
+
+        private static ScaleParameters CreateScaleParameters(ScaleType xScaleType, ScaleType yScaleType)
+        {
+            return new ScaleParameters()
+            {
+                X = new AxisScaleParameters() { ScaleType = xScaleType },
+                Y = new AxisScaleParameters() { ScaleType = yScaleType }
+            };
         }
     }
 }
