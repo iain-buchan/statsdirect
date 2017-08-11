080013e6c40b9d7bef978362c5233ea8cff5ad67
Peter Crowther <peter.crowther@melandra.com>
2017-08-11 17:28:04 +0000

    Prepare 3.1.8

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.1.7.0")]
-[assembly: AssemblyFileVersion("3.1.7.0")]
+[assembly: AssemblyVersion("3.1.8.0")]
+[assembly: AssemblyFileVersion("3.1.8.0")]
 [assembly: CLSCompliant(false)]
