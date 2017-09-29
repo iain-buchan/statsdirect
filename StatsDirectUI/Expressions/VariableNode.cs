e56c0b2a3be3c5f55d3dd67d9435ade2b8969e6a
Peter Crowther <peter.crowther@melandra.com>
2017-09-29 13:57:57 +0000

    #1338: Meaningful exception when the user tries to reference a variable that's not been supplied

--- StatsDirectUI/Expressions/VariableNode.cs
+++ StatsDirectUI/Expressions/VariableNode.cs
@@ -1,4 +1,6 @@
-﻿namespace StatsDirect.Expressions
+﻿using System;
+
+namespace StatsDirect.Expressions
 {
     public class VariableNode : INode
     {
@@ -7,6 +9,8 @@
         public DataType DataType(DataType[] passedVariableTypes)
         {
             // Index is 1-based, array indices in C# (hence passedVariableTypes) start from 0.
+            if (passedVariableTypes.Length < Index)
+                throw new Exception("You can only use a variable that will be passed into the expression. You've used variable " + Index + ", but only " + passedVariableTypes.Length + " variable(s) will be passed in.");
             return passedVariableTypes[Index - 1];
         }
 
