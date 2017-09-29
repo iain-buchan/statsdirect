e09480dc8b8501c14729e774b26f9a67509a4fc2
Peter Crowther <peter.crowther@melandra.com>
2017-09-29 16:16:16 +0000

    Prepare to release 3.1.11

--- StatsDirectUI/Properties/AssemblyInfo.cs
+++ StatsDirectUI/Properties/AssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.1.9.0")]
-[assembly: AssemblyFileVersion("3.1.9.0")]
+[assembly: AssemblyVersion("3.1.11.0")]
+[assembly: AssemblyFileVersion("3.1.11.0")]
 [assembly: CLSCompliant(false)]
