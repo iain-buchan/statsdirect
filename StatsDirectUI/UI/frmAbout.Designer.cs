86fa57a19cece1e01f2478583cdbc29b5502b2db
Peter Crowther <peter.crowther@melandra.com>
2020-02-14 14:03:37 +0000

    Update copyrights to 2020

--- StatsDirectUI/UI/frmAbout.Designer.cs
+++ StatsDirectUI/UI/frmAbout.Designer.cs
@@ -68,7 +68,7 @@
             this.lblCopyright.Name = "lblCopyright";
             this.lblCopyright.Size = new System.Drawing.Size(285, 13);
             this.lblCopyright.TabIndex = 1;
-            this.lblCopyright.Text = "Copyright ©1990-2019 StatsDirect Ltd.  All rights reserved.";
+            this.lblCopyright.Text = "Copyright ©1990-2020 StatsDirect Ltd.  All rights reserved.";
             // 
             // cmdOK
             // 
