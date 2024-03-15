25b0837614db1901dfab8ea37b3507526958c712
Richard Davies <richard.davies2@liverpool.ac.uk>
2024-03-15 15:26:02 +0000

    Transcription error from VB in Exact22K - replaced 'else' with 'if ( iErr != )'

--- StatsDirectUI/Builtins/ExactBB.cs
+++ StatsDirectUI/Builtins/ExactBB.cs
@@ -464,6 +464,7 @@ namespace StatsDirect.Builtins
             //   Tables(i).informative = (d(0) * d(3) <> 0) Or (d(1) * d(2) <> 0)
 
             Host = host;
+            //cMLE = 0.0;
 
             //  Make sure that exact calculations can be performed
             UseLogScale = useLogScale;
@@ -499,7 +500,7 @@ namespace StatsDirect.Builtins
             }
             if (ierr == 0)
                 cMLE = CalcCmle(1.0, ref ierr);
-            else
+            if (ierr != 0)
             {
                 UseLogScale = true;
                 CalcPoly(dataType, lowerBound, numTables, tables, out ierr);
@@ -510,6 +511,7 @@ namespace StatsDirect.Builtins
             }
             if (ierr == 0)
             {
+                cMLE = 0.0;
                 upFishLim = CalcExactLim(false, true, cMLE, confLevel, ref ierr);
                 loFishLim = CalcExactLim(true, true, cMLE, confLevel, ref ierr);
                 upMidPLim = CalcExactLim(false, false, cMLE, confLevel, ref ierr);
