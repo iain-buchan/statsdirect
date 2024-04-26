8373353653a4226eb64df9ed50187f241ff82cf1
Rachel Crowther <rachel@ozzard.org>
2024-04-26 20:53:53 +0100

    Sign with SHA2-256, not obsolete SHA-1

--- Key/sign.cmd
+++ Key/sign.cmd
@@ -1,4 +1,4 @@
 cd /D "%~dp0"
 set target="%1"
 set password="%STATSDIRECT_SIGNING_PASSWORD%"
-signtool\signtool.exe sign /v /f sdsign.pfx /t http://timestamp.digicert.com /p %password% %1
+signtool\signtool.exe sign /v /f sdsign.pfx /fd SHA256 /t http://timestamp.digicert.com /p %password% %1
